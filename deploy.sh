b50d4ff7347728d1955451fa0da42d8a7791b6e9
guboc11 <guboc11@gmail.com>
2024-10-10 16:52:23 +0900

    Update deploy code

--- deploy.sh
+++ deploy.sh
@@ -66,4 +66,5 @@ sleep 3
 # save the chain_fusion canisters evm address
 export EVM_ADDRESS=$(dfx canister call chain_fusion get_evm_address | awk -F'"' '{print $2}')
 # deploy the contract passing the chain_fusion canisters evm address to receive the fees and create a couple of new jobs
-forge script script/Coprocessor.s.sol:MyScript --fork-url http://localhost:8545 --broadcast --sig "run(address)" $EVM_ADDRESS
+# forge script script/Coprocessor.s.sol:MyScript --fork-url http://localhost:8545 --broadcast --sig "run(address)" $EVM_ADDRESS
+forge script script/Chat.s.sol:MyScript --fork-url http://localhost:8545 --broadcast --sig "run(address)" $EVM_ADDRESS
